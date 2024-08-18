745148528d613702598a815ea6a68e8bb0f26c75
Sean Murphy <sean@iamseanmurphy.com>
2024-08-18 18:35:12 +0000

    Updated themes

--- vantage/functions.php
+++ vantage/functions.php
@@ -6,7 +6,7 @@
  *
  * @license GPL 2.0
  */
-define( 'SITEORIGIN_THEME_VERSION', '1.20.23' );
+define( 'SITEORIGIN_THEME_VERSION', '1.20.24' );
 define( 'SITEORIGIN_THEME_JS_PREFIX', '.min' );
 define( 'SITEORIGIN_THEME_CSS_PREFIX', '.min' );
 
