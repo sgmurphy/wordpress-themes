491ee08fab742b2ce661e36da273ad99a5a8669a
Sean Murphy <sean@iamseanmurphy.com>
2024-08-27 18:37:04 +0000

    Updated themes

--- sydney/js/functions.js
+++ sydney/js/functions.js
@@ -196,6 +196,31 @@ var sydney = sydney || {};
 				menuItem.classList.toggle( 'focus' );
 			}
 		}
+
+		//handle aria-expanded for dropdowns
+		const dropdowns = document.querySelectorAll('.menu-item-has-children > a');
+
+		// Function to toggle aria-expanded
+		function toggleAriaExpanded(event) {
+			event.preventDefault(); // Prevent default behavior on click
+			const expanded = this.getAttribute('aria-expanded') === 'true';
+			this.setAttribute('aria-expanded', !expanded);
+		}
+
+		dropdowns.forEach(dropdown => {
+			// Toggle aria-expanded on click
+			dropdown.addEventListener('click', toggleAriaExpanded);
+
+			// Toggle aria-expanded on hover (mouse enter)
+			dropdown.addEventListener('mouseenter', function() {
+				this.setAttribute('aria-expanded', 'true');
+			});
+
+			// Collapse the menu when mouse leaves the menu item
+			dropdown.addEventListener('mouseleave', function() {
+				this.setAttribute('aria-expanded', 'false');
+			});
+		});		
 	},
 };
 
